acbfaa62b13612a70bded84a7afec4f51da0f450
joonas.fi <pompo500@xs.fi>
2017-01-31 15:15:54 +0200

    Add progress indicators

--- build.sh
+++ build.sh
@@ -2,12 +2,17 @@
 
 #build_inside_docker_image=golang:1.8-alpine
 
+echo "Installing git"
+
 apk add --no-cache git
 
 cd src/
 
-# download dependencies, but don't install
+echo "Downloading dependencies (but not installing)"
+
 go get -d
+
+echo "Compiling"
 
 # compile statically, so this works on Alpine as well
 CGO_ENABLED=0 go build --ldflags '-extldflags "-static"' docserver.go
