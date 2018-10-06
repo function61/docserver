c0c49a211d70d58edba30a4a484ca76e390baf3a
Joonas Loppi <joonas@joonas.fi>
2018-10-06 17:02:10 +0300

    Update buildkit

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM alpine:3.8
 
-CMD docserver
+CMD ["docserver"]
 
 # we connect to S3 over HTTPS
 RUN apk add --no-cache ca-certificates \
 	&& mkdir -p /docserver/_packages
 
-ADD docserver /usr/local/bin/docserver
+ADD rel/docserver_linux-amd64 /usr/local/bin/docserver
--- build.sh
+++ build.sh
@@ -1,49 +1,9 @@
 #!/bin/bash -eu
 
-run() {
-	fn="$1"
+source /build-common.sh
 
-	echo "# $fn"
+BINARY_NAME="docserver"
+COMPILE_IN_DIRECTORY="cmd/docserver"
+GOFMT_TARGETS="main.go"
 
-	"$fn"
-}
-
-downloadDependencies() {
-	dep ensure
-}
-
-checkFormatting() {
-	# unfortunately we need to list formattable directories because "." would include vendor/
-	local offenders=$(ls *.go | xargs gofmt -l)
-
-	if [ ! -z "$offenders" ]; then
-		>&2 echo "formatting errors: $offenders"
-		exit 1
-	fi
-}
-
-unitTests() {
-	go test ./...
-}
-
-staticAnalysis() {
-	go vet ./...
-}
-
-buildLinuxAmd64() {
-	# compile statically so this works on Alpine that doesn't have glibc
-	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$FRIENDLY_REV_ID -extldflags \"-static\"" -o docserver
-}
-
-rm -rf rel
-mkdir rel
-
-run downloadDependencies
-
-run checkFormatting
-
-run staticAnalysis
-
-run unitTests
-
-run buildLinuxAmd64
+standardBuildProcess
